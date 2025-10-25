6373424bbc8a9b0b7cfbc3ff768eb9cf2e2d2384
saikonuru <sairam.konuru@gmail.com>
2025-10-26 00:51:53 +0530

    Enable CORS

--- index.ts
+++ index.ts
@@ -1,25 +1,26 @@
-import cors, {CorsOptions} from 'cors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes';
 dotenv.config();
 
-const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS?.split(',') || [];
-
-const corsOptions: CorsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-};
-
 const app = express();
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+      if (!origin || origin.startsWith('http://localhost')) {
+        return callback(null, true);
+      }
+      const allowed = ['https://ai-app-client.onrender.com'];
+      if (allowed.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(router);
 const port = process.env.PORT || 3000;
